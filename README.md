71528723545cc20fa212143518eb46c2d0468905
winterdelta <winterdelta@users.noreply.github.com>
2022-09-15 22:36:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-dfczsv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-dfczsv)
---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Hello!');
+
+let board = [[1], [2], [3]];
+
+console.log(board);
+
+const value = 'x';
+const colIndex = 1;
+
+board[2] = value;
+console.log(board);
